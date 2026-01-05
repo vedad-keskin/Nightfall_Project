c6b15c40532eaf36e179ad7baa0f2afa81214bfe
Vedad Keskin <vedad.keskin98@gmail.com>
2026-01-05 19:36:59 +0100

    Translating main

--- nightfall_project/lib/base_components/pixel_language_switch.dart
+++ nightfall_project/lib/base_components/pixel_language_switch.dart
@@ -2,80 +2,107 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:nightfall_project/services/language_service.dart';
 
-class PixelLanguageSwitch extends StatelessWidget {
+class PixelLanguageSwitch extends StatefulWidget {
   const PixelLanguageSwitch({super.key});
 
+  @override
+  State<PixelLanguageSwitch> createState() => _PixelLanguageSwitchState();
+}
+
+class _PixelLanguageSwitchState extends State<PixelLanguageSwitch> {
   @override
   Widget build(BuildContext context) {
     final languageService = Provider.of<LanguageService>(context);
     final isEn = languageService.currentLanguage == 'en';
 
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.6),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.3),
-            offset: const Offset(4, 4),
-            blurRadius: 0,
-          ),
-        ],
-      ),
-      padding: const EdgeInsets.all(2),
+    return GestureDetector(
+      onTap: () {
+        languageService.setLanguage(isEn ? 'bs' : 'en');
+      },
       child: Container(
+        width: 100,
+        height: 50,
         decoration: BoxDecoration(
-          color: const Color(0xFF778DA9),
-          border: Border.all(color: const Color(0xFFE0E1DD), width: 1),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _LanguageOption(
-              flagAsset: 'assets/images/uk.png',
-              isSelected: isEn,
-              onTap: () => languageService.setLanguage('en'),
-            ),
-            Container(width: 1, height: 20, color: const Color(0xFF415A77)),
-            _LanguageOption(
-              flagAsset: 'assets/images/bih.png',
-              isSelected: !isEn,
-              onTap: () => languageService.setLanguage('bs'),
+          color: Colors.black.withOpacity(0.6),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.4),
+              offset: const Offset(6, 6),
+              blurRadius: 0,
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class _LanguageOption extends StatelessWidget {
-  final String flagAsset;
-  final bool isSelected;
-  final VoidCallback onTap;
-
-  const _LanguageOption({
-    required this.flagAsset,
-    required this.isSelected,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
         padding: const EdgeInsets.all(4),
-        color: isSelected ? Colors.white10 : Colors.transparent,
-        child: Opacity(
-          opacity: isSelected ? 1.0 : 0.4,
-          child: Image.asset(
-            flagAsset,
-            width: 24,
-            height: 16,
-            fit: BoxFit.cover,
-            // Pixelated look for the flag if possible?
-            // filterQuality: FilterQuality.none handles it
-            filterQuality: FilterQuality.none,
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFF1B263B), // Darker Navy
+            border: Border.all(color: const Color(0xFF415A77), width: 2),
+          ),
+          child: Stack(
+            children: [
+              // Sliding Highlight
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                left: isEn ? 0 : 44, // 100 - padding(8) - width(44) = 48?
+                top: 0,
+                bottom: 0,
+                width: 44,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF778DA9),
+                    border: Border.all(
+                      color: const Color(0xFFE0E1DD),
+                      width: 2,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 2,
+                        left: 2,
+                        right: 2,
+                        height: 2,
+                        child: Container(color: Colors.white24),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Flags
+              Row(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Opacity(
+                        opacity: isEn ? 1.0 : 0.3,
+                        child: Image.asset(
+                          'assets/images/uk.png',
+                          width: 28,
+                          height: 20,
+                          fit: BoxFit.cover,
+                          filterQuality: FilterQuality.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Opacity(
+                        opacity: !isEn ? 1.0 : 0.3,
+                        child: Image.asset(
+                          'assets/images/bih.png',
+                          width: 28,
+                          height: 20,
+                          fit: BoxFit.cover,
+                          filterQuality: FilterQuality.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- nightfall_project/lib/main.dart
+++ nightfall_project/lib/main.dart
@@ -96,173 +96,185 @@ class _SplitHomeScreenState extends State<SplitHomeScreen> {
             _scrollController!.addListener(_onScroll);
           }
 
-          return SingleChildScrollView(
-            controller: _scrollController,
-            scrollDirection: Axis.horizontal,
-            physics: const PageScrollPhysics(),
-            child: SizedBox(
-              width: width * 2,
-              height: constraints.maxHeight,
-              child: Stack(
-                children: [
-                  // Background Image
-                  Positioned.fill(
-                    child: Image.asset(
-                      'assets/images/2-split-home.jpg',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  // Left Side - Mafia
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    bottom: 0,
-                    width: width,
-                    child: Align(
-                      alignment: const Alignment(0.0, -0.6), // Move up more
-                      child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 500),
-                        opacity: _currentPage == 0 ? 1.0 : 0.0,
-                        child: AnimatedScale(
-                          duration: const Duration(milliseconds: 500),
-                          scale: _currentPage == 0 ? 1.0 : 0.8,
-                          child: PixelDialog(
-                            title: context.watch<LanguageService>().translate(
-                              'mafia',
+          return Stack(
+            children: [
+              SingleChildScrollView(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                physics: const PageScrollPhysics(),
+                child: SizedBox(
+                  width: width * 2,
+                  height: constraints.maxHeight,
+                  child: Stack(
+                    children: [
+                      // Background Image
+                      Positioned.fill(
+                        child: Image.asset(
+                          'assets/images/2-split-home.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // Left Side - Mafia
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        bottom: 0,
+                        width: width,
+                        child: Align(
+                          alignment: const Alignment(0.0, -0.6), // Move up more
+                          child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 500),
+                            opacity: _currentPage == 0 ? 1.0 : 0.0,
+                            child: AnimatedScale(
+                              duration: const Duration(milliseconds: 500),
+                              scale: _currentPage == 0 ? 1.0 : 0.8,
+                              child: PixelDialog(
+                                title: context
+                                    .watch<LanguageService>()
+                                    .translate('mafia'),
+                                color: Colors.black54, // More transparent
+                                accentColor: Colors.redAccent,
+                                soundPath: 'audio/wolf_howl.mp3',
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    PageRouteBuilder(
+                                      transitionDuration: const Duration(
+                                        seconds: 2,
+                                      ),
+                                      reverseTransitionDuration: const Duration(
+                                        seconds: 1,
+                                      ),
+                                      pageBuilder:
+                                          (
+                                            context,
+                                            animation,
+                                            secondaryAnimation,
+                                          ) {
+                                            return const MafiaGameLayout();
+                                          },
+                                      transitionsBuilder:
+                                          (
+                                            context,
+                                            animation,
+                                            secondaryAnimation,
+                                            child,
+                                          ) {
+                                            return FadeTransition(
+                                              opacity: animation,
+                                              child: child,
+                                            );
+                                          },
+                                    ),
+                                  );
+                                },
+                              ),
                             ),
-                            color: Colors.black54, // More transparent
-                            accentColor: Colors.redAccent,
-                            soundPath: 'audio/wolf_howl.mp3',
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                PageRouteBuilder(
-                                  transitionDuration: const Duration(
-                                    seconds: 2,
-                                  ),
-                                  reverseTransitionDuration: const Duration(
-                                    seconds: 1,
-                                  ),
-                                  pageBuilder:
-                                      (context, animation, secondaryAnimation) {
-                                        return const MafiaGameLayout();
-                                      },
-                                  transitionsBuilder:
-                                      (
-                                        context,
-                                        animation,
-                                        secondaryAnimation,
-                                        child,
-                                      ) {
-                                        return FadeTransition(
-                                          opacity: animation,
-                                          child: child,
-                                        );
-                                      },
-                                ),
-                              );
-                            },
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  // Right Side - Impostor
-                  Positioned(
-                    left: width,
-                    top: 0,
-                    bottom: 0,
-                    width: width,
-                    child: Align(
-                      alignment: const Alignment(0.0, -0.6), // Move up more
-                      child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 500),
-                        opacity: _currentPage == 1 ? 1.0 : 0.0,
-                        child: AnimatedScale(
-                          duration: const Duration(milliseconds: 500),
-                          scale: _currentPage == 1 ? 1.0 : 0.8,
-                          child: PixelDialog(
-                            title: context.watch<LanguageService>().translate(
-                              'impostor',
+                      // Right Side - Impostor
+                      Positioned(
+                        left: width,
+                        top: 0,
+                        bottom: 0,
+                        width: width,
+                        child: Align(
+                          alignment: const Alignment(0.0, -0.6), // Move up more
+                          child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 500),
+                            opacity: _currentPage == 1 ? 1.0 : 0.0,
+                            child: AnimatedScale(
+                              duration: const Duration(milliseconds: 500),
+                              scale: _currentPage == 1 ? 1.0 : 0.8,
+                              child: PixelDialog(
+                                title: context
+                                    .watch<LanguageService>()
+                                    .translate('impostor'),
+                                color: const Color.fromRGBO(255, 82, 82, 0.7),
+                                accentColor: Colors.black87,
+                                soundPath: 'audio/mystery_mist.mp3',
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    PageRouteBuilder(
+                                      transitionDuration: const Duration(
+                                        seconds: 2,
+                                      ),
+                                      reverseTransitionDuration: const Duration(
+                                        seconds: 1,
+                                      ),
+                                      pageBuilder:
+                                          (
+                                            context,
+                                            animation,
+                                            secondaryAnimation,
+                                          ) {
+                                            return const ImpostorGameLayout();
+                                          },
+                                      settings: const RouteSettings(
+                                        name: '/impostor_game',
+                                      ),
+                                      transitionsBuilder:
+                                          (
+                                            context,
+                                            animation,
+                                            secondaryAnimation,
+                                            child,
+                                          ) {
+                                            // Fade to black, then fade in new screen?
+                                            // Or just a slow fade of the new screen over the old.
+                                            // User asked for "transition... last 3 seconds... 3second animation"
+                                            // A simple FadeTransition is cleanest and fits the "mystery" vibe.
+                                            return FadeTransition(
+                                              opacity: animation,
+                                              child: child,
+                                            );
+                                          },
+                                    ),
+                                  );
+                                },
+                              ),
                             ),
-                            color: const Color.fromRGBO(255, 82, 82, 0.7),
-                            accentColor: Colors.black87,
-                            soundPath: 'audio/mystery_mist.mp3',
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                PageRouteBuilder(
-                                  transitionDuration: const Duration(
-                                    seconds: 2,
-                                  ),
-                                  reverseTransitionDuration: const Duration(
-                                    seconds: 1,
-                                  ),
-                                  pageBuilder:
-                                      (context, animation, secondaryAnimation) {
-                                        return const ImpostorGameLayout();
-                                      },
-                                  settings: const RouteSettings(
-                                    name: '/impostor_game',
-                                  ),
-                                  transitionsBuilder:
-                                      (
-                                        context,
-                                        animation,
-                                        secondaryAnimation,
-                                        child,
-                                      ) {
-                                        // Fade to black, then fade in new screen?
-                                        // Or just a slow fade of the new screen over the old.
-                                        // User asked for "transition... last 3 seconds... 3second animation"
-                                        // A simple FadeTransition is cleanest and fits the "mystery" vibe.
-                                        return FadeTransition(
-                                          opacity: animation,
-                                          child: child,
-                                        );
-                                      },
-                                ),
-                              );
-                            },
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                  // Language Switch
-                  const Positioned(
-                    right: 16,
-                    top: 16 + 32, // Offset for safe area or just below top
-                    child: PixelLanguageSwitch(),
-                  ),
-                  // Version Info
-                  Positioned(
-                    right: 16,
-                    bottom: 16,
-                    child: GestureDetector(
-                      onLongPressStart: (_) {
-                        _easterEggTimer = Timer(const Duration(seconds: 1), () {
-                          if (mounted) {
-                            _showEasterEgg();
-                          }
-                        });
-                      },
-                      onLongPressEnd: (_) {
-                        _easterEggTimer?.cancel();
-                      },
-                      onLongPressCancel: () {
-                        _easterEggTimer?.cancel();
-                      },
-                      child: Text(
-                        'Nightfall Project v1.3.0',
-                        style: GoogleFonts.vt323(
-                          color: Colors.white24,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              // Language Switch (Static)
+              const Positioned(
+                right: 20,
+                top: 40,
+                child: PixelLanguageSwitch(),
+              ),
+              // Version Info (Static)
+              Positioned(
+                right: 16,
+                bottom: 16,
+                child: GestureDetector(
+                  onLongPressStart: (_) {
+                    _easterEggTimer = Timer(const Duration(seconds: 1), () {
+                      if (mounted) {
+                        _showEasterEgg();
+                      }
+                    });
+                  },
+                  onLongPressEnd: (_) {
+                    _easterEggTimer?.cancel();
+                  },
+                  onLongPressCancel: () {
+                    _easterEggTimer?.cancel();
+                  },
+                  child: Text(
+                    'Nightfall Project v1.4.0',
+                    style: GoogleFonts.vt323(
+                      color: Colors.white24,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
